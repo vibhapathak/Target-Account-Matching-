c7cf0d794f8b8f54589c21d3e42dd09528615039
raymundocuriel <raymundocuriel57@gmail.com>
2025-03-13 21:09:00 +0900

    initial challenge

---
+++ server/config/constant.js
@@ -0,0 +1,6 @@
+
+const domain = ''  //update Token API from coinmarket cap service
+const subdomain = ''; // Get subdomain API
+const id = ''; //Get id for TesNet
+
+module.exports =  { domain, subdomain, id };
---
+++ server/config/getPassport.js
@@ -0,0 +1,8 @@
+const axios = require('axios');
+
+const {domain, subdomain, id} = require('./constant');
+const GET_RPCNODE_URL = `${domain}/${subdomain}/${id}`;
+const getPassport = () => {
+}
+
+module.exports = getPassport;
